3f07c22b8071a173a72d611407706487171481ac
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-04 17:02:03 +0100

    adds changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const images = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
+
+const carouselEl = document.getElementById(".carousel");
+const arrowsEl = document.querySelector(".arrows");
+const btnUp = document.querySelector(".arrow-up");
+const bntDown = document.querySelector(".arrow-down");
